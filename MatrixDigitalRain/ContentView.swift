67d4903b551abba7934da6c2d8a3cc348cb95375
Aleksey Shevchenko <alekseiwe@gmail.com>
2023-01-03 19:39:58 +0000

    Initial Commit

---
+++ MatrixDigitalRain/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MatrixDigitalRain
+//
+//  Created by Aleksey Shevchenko on 03.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatrixDigitalRain/MatrixDigitalRainApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixDigitalRainApp.swift
+//  MatrixDigitalRain
+//
+//  Created by Aleksey Shevchenko on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MatrixDigitalRainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
