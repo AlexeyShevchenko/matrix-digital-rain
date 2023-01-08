51d361782567d8cb7ff5466364b2907db6c9394c
Aleksey Shevchenko <alekseiwe@gmail.com>
2023-01-08 16:39:23 +0000

    MDR-001: Scaffolds

--- MatrixDigitalRain/VIews/ContentView.swift
+++ MatrixDigitalRain/VIews/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         DownpourView(.init())
+            .onAppear {
+                UIApplication.shared.isIdleTimerDisabled = true
+            }
     }
 }
 
